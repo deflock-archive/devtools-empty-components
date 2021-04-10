694c90f44c4adba5ab51e1f77a724fb41a5a27ff
deflock <deflocked@yandex.com>
2021-04-11 02:56:01 +0300

    Initial

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Hello() {
+  return <div>Hello world</div>;
+}
+
+ReactDOM.render(<Hello/>, document.getElementById("root"));
